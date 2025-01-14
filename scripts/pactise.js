c0b792125e969a8489a145d080732ee5d6bb5d62
christian-mapundu <Christianmapundu@gmail.com>
2025-01-14 13:29:23 +0200

    a lot of changes made and to the index and practise file.

--- scripts/pactise.js
+++ scripts/pactise.js
@@ -1,15 +1,3 @@
-const full = document.querySelector("#full");
-const currentYear = new Date().getFullYear();
-document.getElementById("currentYear").textContent = currentYear;
-full.innerHTML = `Today is <span class="highlight">${new Intl.DateTimeFormat(
-	"en-US",
-	{
-		dateStyle: "full"
-	}
-).format(today)}</span>`;
-
-
-
 const courses = [
   { subject: 'CSE', number: 110, title: 'Introduction to Programming', credits: 2, certificate: 'Web and Computer Programming', description: 'This course will introduce students to programming. It will introduce the building blocks of programming languages (variables, decisions, calculations, loops, array, and input/output) and use them to solve problems.', technology: [ 'Python' ], completed: false },
   { subject: 'WDD', number: 130, title: 'Web Fundamentals', credits: 2, certificate: 'Web and Computer Programming', description: 'This course introduces students to the World Wide Web and to careers in web site design and development. The course is hands on with students actually participating in simple web designs and programming. It is anticipated that students who complete this course will understand the fields of web design and development and will have a good idea if they want to pursue this degree as a major.', technology: [ 'HTML', 'CSS' ], completed: false },
