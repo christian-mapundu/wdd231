6ceafa474aed257b1374cb1f1285dae96ac63b23
christian-mapundu <Christianmapundu@gmail.com>
2025-01-24 13:22:28 +0200

    added additional information to the footer like javascript and staff.

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,3 +10,16 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
 	hamburger.classList.remove("active");
 	navMenu.classList.remove("active");
 }))
+
+
+
+
+function updateDateTime() {
+    const now = new Date();
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    const formattedDateTime = now.toLocaleString('en-US', options);
+    document.getElementById("lastModified").innerHTML = formattedDateTime; 
+  }
+
+  // Initial update
+  updateDateTime();
