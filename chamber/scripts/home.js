9fd40397c0bdd16cb162018a9dc337c9795572e5
christian-mapundu <Christianmapundu@gmail.com>
2025-02-04 23:46:56 +0200

    ...

--- chamber/scripts/home.js
+++ chamber/scripts/home.js
@@ -1,3 +1,20 @@
+// JavaScript
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+	hamburger.classList.toggle("active");
+	navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+	hamburger.classList.remove("active");
+	navMenu.classList.remove("active");
+}))
+
+
+
+// Events Section using fetch to get data about Timbuktu events
 document.addEventListener("DOMContentLoaded", async function () {
     const eventsContainer = document.querySelector(".current-events .event");
 
